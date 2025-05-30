dd53b2b4aa830dc11eefe31a3a6fea1cfa8e159b
bbranco8 <bbranco.780@gmail.com>
2025-05-30 01:14:16 +0100

    cg

--- javascript/index_tv.js
+++ javascript/index_tv.js
@@ -5,7 +5,7 @@ const imagens = [
 ];
 let indice_atual = 0;
 
-document.getElementById("play").addEventListener("click", function () {
+document.getElementById("play_tv").addEventListener("click", function () {
     indice_atual = (indice_atual + 1) % imagens.length; // incrementa e volta ao início quando passar do último
     document.getElementById("tv_on").src = imagens[indice_atual];
 });
